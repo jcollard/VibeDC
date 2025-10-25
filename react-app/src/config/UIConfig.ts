6a6b5b091978d168ad608c980c541d8ff527cb2e
Captain Coder <captaincodersacademy@gmail.com>
2025-10-25 13:15:50 -0700

    chore: Remove font files and references.

--- react-app/src/config/UIConfig.ts
+++ react-app/src/config/UIConfig.ts
@@ -9,12 +9,6 @@ export class UIConfig {
   private static _highlightColor = '#ccaa00';
 
   /**
-   * Font used for button text
-   * Default: Bitfantasy
-   */
-  private static _buttonFont = 'Bitfantasy';
-
-  /**
    * Get the current highlight color
    */
   static getHighlightColor(): string {
@@ -26,19 +20,5 @@ export class UIConfig {
    */
   static setHighlightColor(color: string): void {
     this._highlightColor = color;
-  }
-
-  /**
-   * Get the current button font
-   */
-  static getButtonFont(): string {
-    return this._buttonFont;
-  }
-
-  /**
-   * Set the button font
-   */
-  static setButtonFont(font: string): void {
-    this._buttonFont = font;
   }
 }
