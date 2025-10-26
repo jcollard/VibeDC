c81a71f11d415d87a4bcca679ceea47a59d56181
Captain Coder <captaincodersacademy@gmail.com>
2025-10-26 14:11:08 -0700

    chore: Change enter combat text.

--- react-app/src/models/combat/CombatConstants.ts
+++ react-app/src/models/combat/CombatConstants.ts
@@ -65,7 +65,7 @@ export const CombatConstants = {
     DEPLOYMENT_INSTRUCTION: 'Click [sprite:gradients-7] to deploy a unit.',
     SELECT_PARTY_MEMBER: 'Select a [color=#ffa500]Party Member[/color] to deploy',
     START_COMBAT_BUTTON: 'Start Combat',
-    UNITS_DEPLOYED: 'Units deployed. Enter combat?',
+    UNITS_DEPLOYED: '[color=#ffa500]Party Members[/color] ready! Enter combat?',
     STARTING_ENEMY_DEPLOYMENT: 'Starting Enemy Deployment',
     ENEMIES_APPROACH: 'Enemies Approach',
   },
